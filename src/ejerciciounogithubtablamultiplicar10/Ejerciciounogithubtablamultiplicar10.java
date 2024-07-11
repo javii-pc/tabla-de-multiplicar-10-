822f291e4bd9542d4d517a0c0eb5e71c83e24afd
Javier Pérez <javiepr0104@gmail.com>
2024-07-11 12:06:25 -0600

    tabla de multiplicar 10

---
+++ src/ejerciciounogithubtablamultiplicar10/Ejerciciounogithubtablamultiplicar10.java
@@ -0,0 +1,13 @@
+package ejerciciounogithubtablamultiplicar10;
+
+public class Ejerciciounogithubtablamultiplicar10 {
+
+    public static void main(String[] args) {
+        int multi ;
+        for (int i = 1; i <= 10; i++) {
+            multi = 10 * i;
+            System.out.println("10"+"*"+i+"="+multi);
+        }
+    }
+
+}
